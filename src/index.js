d6b683e0acf6af51383b382fe184ae1486c7b8bf
subedigaurav <ergauravsubedi@gmail.com>
2020-07-17 12:03:13 +0545

    bug: remove the withRouter wrapping for the main app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {
-  createStore, applyMiddleware, compose, combineReducers,
-} from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
@@ -16,9 +14,10 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 
-const composeEnhancers =	process.env.NODE_ENV === 'development'
-	  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-	  : null || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
